2858342a8d15cd8c3cfe0641a5e30e715f11533a
Cathalina Murcia <dmaderop094@inemkennedy.edu.co>
2026-05-07 13:52:52 -0500

    2do

--- app.py
+++ app.py
@@ -1,22 +1,29 @@
-# Importamos Flask y una funcion que permite mostrar un HTML.
+
 from flask import Flask, render_template
 
 
-# Creamos la aplicacion principal.
-# Este objeto sera el centro de nuestro proyecto Flask.
 app = Flask(__name__)
 
 
-# Cuando alguien entra a la direccion principal del sitio, Flask ejecuta
-# esta funcion y devuelve la pagina `index.html`.
 @app.route("/")
 def inicio():
-    # `render_template` busca archivos dentro de la carpeta `templates`.
     return render_template("index.html")
 
 
-# Este bloque se ejecuta solo si corremos `python app.py` desde la terminal.
+@app.route("/chao")
+def chao(): 
+    return render_template("chao.html")
+
+
+@app.route("/hola")
+def hola(): 
+    return render_template("hola.html")
+
+
+@app.route("/css")
+def css():
+    return render_template("css.html")
+
 if __name__ == "__main__":
-    # `debug=True` sirve en desarrollo porque reinicia el servidor
-    # cuando detecta cambios y muestra errores con mas detalle.
+
     app.run(debug=True)
